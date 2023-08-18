7218d0eeefc55e977cb35f8195cddae8f7038fa0
Elena <elenadiaz32505@gmail.com>
2023-08-18 15:18:32 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,97 @@
+const https = require('https');
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+var cors = require('cors');
+const fs = require('fs');
+const creds = require('./config');
+const serverPort = 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/', router);
+
+app.get('/', (req, res) => {
+	res.json('hi');
+});
+
+var transport = {
+	host: creds.HOST,
+	port: creds.MAILPORT,
+	auth: {
+		user: creds.USER,
+		pass: creds.PASS,
+	},
+	from: creds.EMAIL,
+};
+
+var transporter = nodemailer.createTransport(transport);
+transporter.verify((error, success) => {
+	if (error) {
+		console.log(error);
+	} else {
+		console.log('Server is ready to take messages');
+	}
+});
+
+router.post('/send', (req, res, next) => {
+	var name = req.body.name;
+	var email = req.body.email;
+	var message = req.body.message;
+	var senderEmail = `${name} <${creds.EMAIL}>`;
+	var yourEmail = `${creds.YOURNAME} <${creds.EMAIL}>`;
+	var content = `name: ${name} \n email: ${email} \n message: ${message} `;
+	var mail = {
+		from: senderEmail,
+		to: creds.EMAIL, // This is email address where you will receive messages
+		subject: `New Portfolio Message from ${name}`,
+		text: content,
+	};
+
+	transporter.sendMail(mail, (err, data) => {
+		console.log(err);
+		console.log(data);
+		if (err) {
+			res.json({
+				status: 'fail',
+			});
+		} else {
+			res.json({
+				status: 'success',
+			});
+
+			//Send Auto Reply email
+			transporter.sendMail(
+				{
+					from: yourEmail,
+					to: email,
+					subject: 'Message received',
+					text: `Hi ${name},\nThank you for sending me a message. I will get back to you soon.\n\nBest Regards,\n${creds.YOURNAME}\n${creds.YOURSITE}\n\n\nMessage Details\nName: ${name}\n Email: ${email}\n Message: ${message}`,
+					html: `<p>Hi ${name},<br>Thank you for sending me a message. I will get back to you soon.<br><br>Best Regards,<br>${creds.YOURNAME}<br>${creds.YOURSITE}<br><br><br>Message Details<br>Name: ${name}<br> Email: ${email}<br> Message: ${message}</p>`,
+				},
+				function (error, info) {
+					if (error) {
+						console.log(error);
+					} else {
+						console.log('Message sent: ' + info.response);
+					}
+				}
+			);
+		}
+	});
+});
+
+//Create https server, make sure to set the path to the certificates
+const httpsServer = https.createServer(
+	{
+		key: fs.readFileSync('path-to-privkey.pem'),
+		cert: fs.readFileSync('path-to-cert.pem'),
+	},
+	app
+);
+httpsServer.listen(serverPort, () =>
+	console.log(`backend is running on port ${serverPort}`)
+);
+
+//app.listen(serverPort, () => console.log(`backend is running on port ${serverPort}`));
